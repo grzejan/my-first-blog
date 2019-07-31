f39eb05a20adef6820b8adcb92c6e2218f3b5549
grzejan <grzejan@gmail.com>
2019-07-31 17:01:19 +0200

    Added: drafts, publish draft and delete post

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>', views.post_detail, name='post_detail'),
-    path('post/new', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
+    path('posts/<int:pk>', views.post_detail, name='post_detail'),
+    path('posts/new', views.post_new, name='post_new'),
+    path('posts/<int:pk>/edit', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('posts/<int:pk>/publish', views.post_publish, name='post_publish'),
+    path('posts/<int:pk>/remove', views.post_remove, name='post_remove'),
 ]
